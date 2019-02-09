7d3db6905f86e6a14291f58bfcd5bcc52b44950d
CountBlueberry <41909238+CountBlueberry@users.noreply.github.com>
2019-02-09 21:17:35 +0200

    Updating

---
+++ MonstarClicker/Assets/Scripts/ButtonHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHelper : MonoBehaviour
+{
+    public bool isPoused = false;
+    public bool isTurnedOf = false;
+
+    public GameObject shopPanel;
+    public DamageHelper damageHelper;
+
+
+
+    void Start()
+    {
+        damageHelper = GameObject.FindObjectOfType<DamageHelper>();
+    }
+
+    public void Shop()
+    {
+        if (shopPanel.gameObject.activeSelf)
+        {
+            isTurnedOf = true;
+            shopPanel.GetComponent<Animator>().SetTrigger("ShopWindowUp");
+            shopPanel.gameObject.SetActive(false);
+            isPoused = false;
+        }
+        else
+        {
+            isPoused = true;
+            shopPanel.SetActive(true);
+            shopPanel.layer = LayerMask.NameToLayer("ShopLayer");
+            shopPanel.GetComponent<Animator>().SetTrigger("ShopWindowUp");
+        }
+    }
+
+    public void FirstUpgrade()
+    {
+        int firstWeapon = 1;
+        damageHelper.SetDamageFunction(firstWeapon);
+    }
+}
---
+++ MonstarClicker/Assets/Scripts/TimeHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class TimeHelper : MonoBehaviour{
+
+    private float startTime;
+    private float endTime = 2.0f;
+
+    private GameHelper gameHelper;
+    private ButtonHelper buttonHelper;
+
+    void Start()
+    {
+        gameHelper = GameObject.FindObjectOfType<GameHelper>();
+        buttonHelper = GameObject.FindObjectOfType<ButtonHelper>();
+    }
+
+    void Update()
+    {
+
+        if (gameHelper.isDead == true)
+        {
+            startTime += 0.8f * Time.deltaTime;
+
+            if (startTime >= endTime)
+            {
+                startTime = 0;
+                gameHelper.SpawnMonster();
+            }
+        }
+
+        if (buttonHelper.isTurnedOf == true)
+        {
+            startTime += 0.8f * Time.deltaTime;
+
+            if (startTime >= endTime)
+            {
+                startTime = 0;
+                buttonHelper.isTurnedOf = false;
+            }
+        }
+    }
+}
